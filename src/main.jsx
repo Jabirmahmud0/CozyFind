db5ce91b094185caec78470776c3163e1235f177
unknown <mahmud15-5194@diu.edu.bd>
2025-11-01 14:25:14 +0600

    Simplify vercel.json configuration for static site deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,16 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import router from './Routes/Router.jsx'
 import { RouterProvider } from 'react-router-dom'
-import FirebaseProvider from './FirebaseProvider/FirebaseProvider.jsx';
-import { ToastContainer } from 'react-toastify';
+import FirebaseProvider from './FirebaseProvider/FirebaseProvider.jsx'
+import { ToastContainer } from 'react-toastify'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <FirebaseProvider>
-      <RouterProvider router={router}>
-        <ToastContainer />
-      </RouterProvider>
+      <RouterProvider router={router} />
+      <ToastContainer />
     </FirebaseProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
